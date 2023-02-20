3e07ab45213d14348e0e2c7a73fad9061bda8a84
Patel harsh <harshmpatel0155@gmail.com>
2023-02-20 23:16:02 +0530

    Initial Commit

---
+++ githubpython/githubpythonApp.swift
@@ -0,0 +1,17 @@
+//
+//  githubpythonApp.swift
+//  githubpython
+//
+//  Created by Patel Harsh on 20/02/23.
+//
+
+import SwiftUI
+
+@main
+struct githubpythonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
