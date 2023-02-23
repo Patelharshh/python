ee572e66703d1499df77d66021b1147dd9269970
Patel harsh <harshmpatel0155@gmail.com>
2023-02-23 19:38:18 +0530

    swapcase

---
+++ .vscode/stringswapcasee.py
@@ -0,0 +1,12 @@
+name = "PaTeL HaRsH"
+print(name.swapcase())
+
+
+sentence1 = "I LOVE BIKES"
+print(sentence1.swapcase())
+
+sentence2 = "i hate bikes "
+print(sentence2.swapcase())
+
+sentence3 = "i HaVe biKe"
+print(sentence3.swapcase())
